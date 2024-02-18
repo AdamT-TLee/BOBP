b1addcfa5a264ee462d2d6186006f9f85432860b
alx-yng <alexyoung0119@gmail.com>
2024-02-18 02:24:07 -0600

    Finished Dashboard Demo

--- pet-app/src/components/googleMaps.js
+++ pet-app/src/components/googleMaps.js
@@ -1,4 +1,13 @@
 const googleMaps = () => {
+  const listItems = document.querySelectorAll(".reminders-list-item");
+
+  // Add click event listener to each list item
+  listItems.forEach((item) => {
+    item.addEventListener("click", () => {
+      item.classList.toggle("checked");
+    });
+  });
+
   var mapProp = {
     center: new google.maps.LatLng(51.508742, -0.12085),
     zoom: 5,
