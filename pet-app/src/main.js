a0863d4e390d8e6a83e958c66dc80b755401fca7
alx-yng <alexyoung0119@gmail.com>
2024-02-17 20:20:29 -0600

    first front end commit

---
+++ pet-app/src/main.js
@@ -0,0 +1,5 @@
+import "../styles/modern-normalize.css";
+import "../styles/style.css";
+import "../styles/landing/background.css";
+import "../styles/landing/form.css";
+import "../styles/utils.css";
